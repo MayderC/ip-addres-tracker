593b5560beeaccca081763af5c976b5c0ee7e910
Abs <44930667+MayderC@users.noreply.github.com>
2020-12-29 23:12:27 -0600

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -8,7 +8,7 @@ exports.controllerHome = (req, res) => {
 var cords = ""
 exports.getPosition = (req, res) => {
 	const https = require("https");
-	const api_key = "at_mntyxTdkytOSafPeWhIxx6nEhPjeg";
+	const api_key = "";
 	const api_url = "https://geo.ipify.org/api/v1?";
 	let ip = req.body.ip;
 	let url = `${api_url}apiKey=${api_key}&ipAddress=${ip}`;
@@ -28,7 +28,7 @@ exports.getPosition = (req, res) => {
 						lng: str["location"]["lng"],
 						region : str['location']['region'],
 						city: str['location']['city'],
-						key: "pk.eyJ1IjoiZGF2aWRhYnMiLCJhIjoiY2tmZzU4cGZpMDZwcTJxbzR3ZTc0ZWEybiJ9.O3PvZzSBzRfVSdVEjf6sFQ"
+						key: ""
 					};
 					cords = obj;
 				}else{
