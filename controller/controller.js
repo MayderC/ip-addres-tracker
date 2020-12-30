b171b325b014bd89bbe73b7d63e9042fb48f8c9d
MayderC <maydercortes1@gmail.com>
2020-12-29 23:02:26 -0600

    completed

--- controller/controller.js
+++ controller/controller.js
@@ -8,7 +8,7 @@ exports.controllerHome = (req, res) => {
 var cords = ""
 exports.getPosition = (req, res) => {
 	const https = require("https");
-	const api_key = "";
+	const api_key = "at_mntyxTdkytOSafPeWhIxx6nEhPjeg";
 	const api_url = "https://geo.ipify.org/api/v1?";
 	let ip = req.body.ip;
 	let url = `${api_url}apiKey=${api_key}&ipAddress=${ip}`;
@@ -21,7 +21,6 @@ exports.getPosition = (req, res) => {
 			});
 			response.on("end", () => {
 				str = JSON.parse(str);
-				console.log(str)
 				
 				if(str){
 					let obj = {
@@ -29,7 +28,7 @@ exports.getPosition = (req, res) => {
 						lng: str["location"]["lng"],
 						region : str['location']['region'],
 						city: str['location']['city'],
-						key: ""
+						key: "pk.eyJ1IjoiZGF2aWRhYnMiLCJhIjoiY2tmZzU4cGZpMDZwcTJxbzR3ZTc0ZWEybiJ9.O3PvZzSBzRfVSdVEjf6sFQ"
 					};
 					cords = obj;
 				}else{
